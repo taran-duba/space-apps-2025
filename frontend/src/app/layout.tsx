e3ccb914b4b4a109f9483198c3e1b165d0daebdc
Taran Duba <taran.duba3@gmail.com>
2025-11-01 11:32:54 -0400

    google adsense account

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="google-adsense-account" content="ca-pub-9011514285034166"></meta>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
         style={{
